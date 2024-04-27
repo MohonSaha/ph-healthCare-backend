9e008eb2b4ee77a87979dcf312361fcfe98dc1df
MohonSaha <practicewithmohon@gmail.com>
2024-04-27 11:10:27 +0600

    Retrieving My Appointments As Doctor Or Patient

--- src/app/modules/appointment/appointment.route.ts
+++ src/app/modules/appointment/appointment.route.ts
@@ -5,6 +5,12 @@ import { AppointmentController } from "./appointment.controller";
 
 const router = express.Router();
 
+router.get(
+  "/my-appointment",
+  auth(UserRole.PATIENT, UserRole.DOCTOR),
+  AppointmentController.getMyAppointment
+);
+
 router.post(
   "/",
   auth(UserRole.PATIENT),
